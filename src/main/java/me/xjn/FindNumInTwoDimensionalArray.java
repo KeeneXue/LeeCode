7ca6dd4081c225a391f849c13da6e503fa5c4674
xuejiannan <xue.jiannan@trs.com.cn>
2020-01-09 17:39:23 +0800

    first commit

---
+++ src/main/java/me/xjn/FindNumInTwoDimensionalArray.java
@@ -0,0 +1,26 @@
+package me.xjn;
+
+/**
+ * @author Xuejn
+ * @version 1.0
+ * @className FindNumInTwoDimensionalArray
+ * @description 在一个二位数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，每一列都按照从左到右递增的顺序排序。
+ * 完成一个函数，输入这样的二维数组和一个整数。判断数组中是否包含该整数。
+ * @date 2020/1/9 16:29
+ **/
+public class FindNumInTwoDimensionalArray {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean solution(int[][] array,int target){
+
+        if(array == null || array.length <= 0 ||
+                array[0] == null || array[0].length <= 0 ){
+            return false;
+        }
+        
+        return false;
+    }
+}
