80b1f24d4d06cb56f5938a00a113ba560e740628
xuejiannan <xue.jiannan@trs.com.cn>
2020-01-10 14:03:44 +0800

    finish find number in two dimensional array

--- src/main/java/me/xjn/FindNumInTwoDimensionalArray.java
+++ src/main/java/me/xjn/FindNumInTwoDimensionalArray.java
@@ -11,16 +11,35 @@ package me.xjn;
 public class FindNumInTwoDimensionalArray {
 
     public static void main(String[] args) {
-
+        int[][] array = new int[5][5];
+        array[0] = new int[]{1,   4,  7, 11, 15};
+        array[1] = new int[]{2,   5,  8, 12, 19};
+        array[2] = new int[]{3,   6,  9, 16, 22};
+        array[3] = new int[]{10, 13, 14, 17, 24};
+        array[4] = new int[]{18, 21, 23, 26, 30};
+        System.out.println(solution(array,20));
     }
 
     public static boolean solution(int[][] array,int target){
 
-        if(array == null || array.length <= 0 ||
-                array[0] == null || array[0].length <= 0 ){
+        if(array == null || array.length == 0 ||
+                array[0] == null || array[0].length == 0 ){
             return false;
         }
-        
+        //m 行数，n 列数
+        int m = array.length,n = array[0].length;
+        //设置开始时元素位置，在这里设置的右上角的元素
+        int i = 0,j = n - 1;
+
+        while (i < m && j >= 0) {
+            if (target > array[i][j]){
+                i++;
+            } else if (target < array[i][j]) {
+                j--;
+            } else {
+                return true;
+            }
+        }
         return false;
     }
 }
